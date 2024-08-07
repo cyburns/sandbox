dcd969f124b56312c37f7304076ab9b6296a9ec6
cyburns <72774499+cyburns@users.noreply.github.com>
2024-08-07 08:48:30 -0400

    ga

--- app/graphql/page.tsx
+++ app/graphql/page.tsx
@@ -1,51 +1,73 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { gql, useQuery } from "@apollo/client";
-import client from "@/lib/apollo-client";
 
-const page = () => {
+import React, { useEffect, useState } from "react";
+import client from "@/lib/apollo-client";
+import { gql, ApolloError } from "@apollo/client";
+
+const GET_GAMES = gql`
+  query GetGames {
+    games {
+      title
+      platform
+      reviews {
+        rating
+        content
+      }
+    }
+  }
+`;
+
+interface Game {
+  id: string;
+  title: string;
+  platform: string[];
+  reviews: {
+    rating: number;
+    content: string;
+  }[];
+}
+
+const Page = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [games, setGames] = useState<any>(null);
+  const [games, setGames] = useState<Game[] | null>(null);
+  const [error, setError] = useState<ApolloError | null>(null);
 
   useEffect(() => {
-    const GET_GAMES = gql`
-      query {
-        games {
-          id
-          title
-          platform
-        }
-      }
-    `;
+    const loadGames = async () => {
+      setIsLoading(true);
+      setError(null);
 
-    setIsLoading(true);
-
-    const fetchGames = async () => {
       try {
         const { data } = await client.query({
           query: GET_GAMES,
+          fetchPolicy: "cache-first",
         });
-
         setGames(data.games);
-      } catch (error) {
+      } catch (error: any) {
+        setError(error);
         console.error("Error getting games:", error);
       } finally {
         setIsLoading(false);
       }
     };
 
-    fetchGames();
+    loadGames();
   }, []);
 
-  if (!games) return <div>Loading...</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading games.</div>;
+  if (!games) return <div>No games found.</div>;
 
   return (
-    <div>
+    <div className="h-screen flex justify-center items-center flex-col">
       <h1>Games</h1>
       <ul>
-        {games.map((game: any) => (
-          <li key={game.id}>
+        {games.map((game) => (
+          <li key={game.id} className="m-3 rounded-lg bg-slate-800 p-10">
             {game.title} - {game.platform.join(", ")}
+            {game.reviews.length > 0 && `(${game.reviews.length} reviews)`}
+            {game.reviews.reduce((acc, review) => acc + review.rating, 0) /
+              game.reviews.length}
           </li>
         ))}
       </ul>
@@ -53,4 +75,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
