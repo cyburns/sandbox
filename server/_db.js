e903e2512d9efb3c4cc60a5d21bb9a718ff2e11a
cyburns <72774499+cyburns@users.noreply.github.com>
2024-08-06 20:21:04 -0400

    startStandaloneServer

--- server/_db.js
+++ server/_db.js
@@ -1,18 +1,18 @@
-let games = [
+const games = [
   { id: "1", title: "Zelda, Tears of the Kingdom", platform: ["Switch"] },
   { id: "2", title: "Final Fantasy 7 Remake", platform: ["PS5", "Xbox"] },
   { id: "3", title: "Elden Ring", platform: ["PS5", "Xbox", "PC"] },
   { id: "4", title: "Mario Kart", platform: ["Switch"] },
-  { id: "5", title: "Pokemon Scarlet", platform: ["PS5", "Xbox", "PC"] },
+  { id: "5", title: "Pokemon Scarconst", platform: ["PS5", "Xbox", "PC"] },
 ];
 
-let authors = [
+const authors = [
   { id: "1", name: "mario", verified: true },
   { id: "2", name: "yoshi", verified: false },
   { id: "3", name: "peach", verified: true },
 ];
 
-let reviews = [
+const reviews = [
   { id: "1", rating: 9, content: "lorem ipsum", author_id: "1", game_id: "2" },
   { id: "2", rating: 10, content: "lorem ipsum", author_id: "2", game_id: "1" },
   { id: "3", rating: 7, content: "lorem ipsum", author_id: "3", game_id: "3" },
--- server/graphql.js
+++ server/graphql.js
@@ -1,33 +1,52 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import db from "./_db.js";
 
 const typeDefs = `#graphql
     type Game {
         id: ID!
         title: String!
         platform: [String!]!
+        reviews: [Review!]
     }
 
     type Review {
         id: ID!
         rating: Int!
         content: String!
+        game: Game!
+        author: Author!
     }
 
     type Author {
         id: ID!
         name: String!
         verified: Boolean!
+        reviews: [Review!]
     }
 
     type Query {
         reviews: [Review]
+        review(id: ID!): Review
         games: [Game]
+        game(id: ID!): Game
         authors: [Author]
+        author(id: ID!): Author
     }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    games: () => db.games,
+    game: (_, args) => db.games.find((game) => game.id === args.id),
+    reviews: () => db.reviews,
+    review: (_, args) => db.reviews.find((review) => review.id === args.id),
+    authors: () => db.authors,
+    author: (_, args) => db.authors.find((author) => author.id === args.id),
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const { url } = await startStandaloneServer(server, {
   listen: {
